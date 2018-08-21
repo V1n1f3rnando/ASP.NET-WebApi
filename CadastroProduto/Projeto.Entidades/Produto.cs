352fcd55e2acb773e4662c9fd9646f1a22858960
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-08-21 16:52:40 -0300

    Criação da camada de serviços e camada de dados.

---
+++ CadastroProduto/Projeto.Entidades/Produto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entidades
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime DataCompra { get; set; }
+
+        //Construtor default
+        public Produto()
+        {
+
+        }
+
+        //Sobrecarregando o construtor
+        public Produto(int idProduto, string nome, decimal preco, int quantidade, DateTime dataCompra)
+        {
+            IdProduto = idProduto;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+            DataCompra = dataCompra;
+        }
+
+        //sobrescrevendo o método ToString da classe object
+        public override string ToString()
+        {
+            //interpolação
+            return $"ID:{IdProduto}\n Nome:{Nome} \n Preço: R${Preco}\n Quantidade:{Quantidade}\n DataCompra:{DataCompra} ";
+        }
+    }
+}

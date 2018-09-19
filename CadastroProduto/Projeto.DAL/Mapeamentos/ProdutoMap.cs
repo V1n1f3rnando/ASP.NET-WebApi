b43e8234619c8b5b601d92616d548be936c0ee2e
Vinicius Fernando <31803742+V1n1f3rnando@users.noreply.github.com>
2018-09-19 16:21:07 -0300

    .

---
+++ CadastroProduto/Projeto.DAL/Mapeamentos/ProdutoMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.Entidades;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Projeto.DAL.Mapeamentos
+{
+    public class ProdutoMap 
+    {
+
+    }
+}
